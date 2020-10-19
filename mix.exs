11f0f4f18e698043b4019d33af959515ca08f6e0
Gertjan Assies <g.assies@newmotion.com>
2020-10-19 19:59:51 +0200

    Added coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule OcppModel.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.json": :test, "coveralls.html": :test]
     ]
   end
 
@@ -23,7 +25,8 @@ defmodule OcppModel.MixProject do
     [
       {:typed_struct, "~> 0.2.1"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:credo, "~> 1.5.0-rc-2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5.0-rc-2", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 end
