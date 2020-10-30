4f8b4d6ad165817c3be70159265e341326ff8402
Gertjan Assies <g.assies@newmotion.com>
2020-10-30 09:08:19 +0100

    better naming

--- lib/ocpp_model/v20/behaviours/charge_system_behaviour.ex
+++ lib/ocpp_model/v20/behaviours/charge_system_behaviour.ex
@@ -28,30 +28,33 @@ defmodule OcppModel.V20.Behaviours.ChargeSystem do
   @doc """
     Main entrypoint, based on the action parameter, this function will call one of the callback functions with the payload
   """
-  def handle(impl, action, payload) when action == "Authorize" do
-    payload |> OcppModel.to_struct(M.AuthorizeRequest) |> impl.authorize |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "Authorize" do
+    payload |> OcppModel.to_struct(M.AuthorizeRequest) |> caller.authorize |> OcppModel.to_map()
   end
 
-  def handle(impl, action, payload) when action == "BootNotification" do
-    payload |> OcppModel.to_struct(M.BootNotificationRequest) |> impl.boot_notification |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "BootNotification" do
+    payload |> OcppModel.to_struct(M.BootNotificationRequest) |> caller.boot_notification |> OcppModel.to_map()
   end
 
-  def handle(impl, action, payload) when action == "DataTransfer" do
-    payload |> OcppModel.to_struct(M.DataTransferRequest) |> impl.data_transfer |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "DataTransfer" do
+    payload |> OcppModel.to_struct(M.DataTransferRequest) |> caller.data_transfer |> OcppModel.to_map()
   end
 
-  def handle(impl, action, payload) when action == "Heartbeat" do
-    payload |> OcppModel.to_struct(M.HeartbeatRequest) |> impl.heartbeat |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "Heartbeat" do
+    payload |> OcppModel.to_struct(M.HeartbeatRequest) |> caller.heartbeat |> OcppModel.to_map()
   end
 
-  def handle(impl, action, payload) when action == "StatusNotification" do
-    payload |> OcppModel.to_struct(M.StatusNotificationRequest) |> impl.status_notification |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "StatusNotification" do
+    payload |> OcppModel.to_struct(M.StatusNotificationRequest) |> caller.status_notification |> OcppModel.to_map()
   end
 
-  def handle(impl, action, payload) when action == "TransactionEvent" do
-    payload |> OcppModel.to_struct(M.TransactionEventRequest) |> impl.transaction_event |> OcppModel.to_map()
+  def handle(caller, action, payload) when action == "TransactionEvent" do
+    payload
+      |> OcppModel.to_struct(M.TransactionEventRequest)
+      |> caller.transaction_event
+      |> OcppModel.to_map()
   end
 
-  def handle(_impl, action, _payload), do: {:error, :unknown_action, "Action #{action} is unknown"}
+  def handle(_caller, action, _payload), do: {:error, :unknown_action, "Action #{action} is unknown"}
 
 end
