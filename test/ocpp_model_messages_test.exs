381fa3487de70a6fb14c3c322c80b7a8c13880db
Gertjan Assies <g.assies@newmotion.com>
2020-11-08 17:03:19 +0100

    added some more property tests

--- test/ocpp_model_messages_test.exs
+++ test/ocpp_model_messages_test.exs
@@ -2,23 +2,23 @@ defmodule OcppModelMessagesTest do
   use ExUnit.Case
   use ExUnitProperties
 
-  import StreamData
+  alias StreamData, as: SD
 
   alias OcppModel.V20.EnumTypes, as: ET
   alias OcppModel.V20.FieldTypes, as: FT
   alias OcppModel.V20.Messages, as: M
 
   defp string_of_len(opt, length) do
-    gen all str <- string(opt), str != "", snipped = String.slice(str, 0..length), do: snipped
+    gen all str <- SD.string(opt), str != "", snipped = String.slice(str, 0..length), do: snipped
   end
 
   defp string_of_enum(enum_type) do
-    gen all value <- StreamData.member_of(ET.get(enum_type)), do: value
+    gen all value <- SD.member_of(ET.get(enum_type)), do: value
   end
 
   defp date_time do
-    gen all year <- integer(2000..2020), month <- integer(1..12 ), day <- integer(1..28),
-            hour <- integer(0..23), minute <- integer(0..59), second <- integer(0..59) do
+    gen all year <- SD.integer(2000..2020), month <- SD.integer(1..12 ), day <- SD.integer(1..28),
+            hour <- SD.integer(0..23), minute <- SD.integer(0..59), second <- SD.integer(0..59) do
       DateTime.new!(Date.new!(year, month, day), Time.new!(hour, minute, second, 0), "Etc/UTC")
         |> DateTime.to_iso8601()
     end
@@ -59,7 +59,26 @@ defmodule OcppModelMessagesTest do
         }
       }
     end
+  end
 
+  defp bootnotification_response do
+    gen all gen_datetime <- date_time() do
+      %M.BootNotificationResponse{
+        currentTime: gen_datetime,
+        interval: SD.integer(1..900),
+        status: string_of_enum(:registrationStatusEnumType),
+        statusInfo: %FT.StatusInfoType{
+          reasonCode: string_of_len(:alphanumeric, 20),
+          additionalInfo: string_of_len(:alphanumeric, 512)
+        }
+      }
+    end
+  end
+
+  defp heartbeat_request do
+    gen all _ <- nil do
+      %M.HeartbeatRequest{}
+    end
   end
 
   defp heartbeat_response do
@@ -72,15 +91,23 @@ defmodule OcppModelMessagesTest do
     check all gen_auth_req <- authorize_request(), do: assert %M.AuthorizeRequest{} = gen_auth_req
   end
 
-  property :boot_notification_request do
-    check all gen_boot_req <- bootnotification_request(), do: assert %M.BootNotificationRequest{} = gen_boot_req
-  end
-
   property :authorize_response do
     check all gen_auth_res <- authorize_response(), do: assert %M.AuthorizeResponse{} = gen_auth_res
   end
 
+  property :boot_notification_request do
+    check all gen_boot_req <- bootnotification_request(), do: assert %M.BootNotificationRequest{} = gen_boot_req
+  end
+
+  property :boot_notification_response do
+    check all gen_boot_res <- bootnotification_response(), do: assert %M.BootNotificationResponse{} = gen_boot_res
+  end
+
+  property :heartbeat_request do
+    check all gen_hb_req <- heartbeat_request(), do: assert %M.HeartbeatRequest{} = gen_hb_req
+  end
+
   property :heartbeat_response do
-    check all gen_heartbeat_res <- heartbeat_response(), do: assert %M.HeartbeatResponse{} = gen_heartbeat_res
+    check all gen_hb_res <- heartbeat_response(), do: assert %M.HeartbeatResponse{} = gen_hb_res
   end
 end
